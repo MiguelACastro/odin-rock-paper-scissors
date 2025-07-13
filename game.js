b92c03ad9bce1088c7ad8d46b26bd79763a1f6fc
Miguel Castro <mcastro_23@alu.uabcs.mx>
2025-07-12 20:33:11 -0700

    Create function to get computer choice

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+function getComputerChoice() {
+    let n = Math.floor(Math.random() * 3)
+    let choice
+    switch (n) {
+        case 0:
+            choice = "rock"
+            break
+        case 1:
+            choice = "paper"
+            break
+        case 2:
+            choice = "scissors"
+            break
+    }
+    return choice
+}
+
+console.log(getComputerChoice())
